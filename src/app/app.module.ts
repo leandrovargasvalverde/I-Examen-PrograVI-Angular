41ae239da4d29bdbf4fa26185f74db9553e10245
leandrovargasvalverde <leandro9700@gmail.com>
2023-03-04 01:02:04 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { CheckEmailComponent } from './components/check-email/check-email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterUserComponent,
+    CheckEmailComponent
   ],
   imports: [
     BrowserModule,
